7c637138cf6e7334062209a4ac275c4e589ccc86
recca <flamerecca711@gmail.com>
2019-08-27 01:12:02 +0800

    Test Line response

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -7,6 +7,9 @@ namespace App\Http\Controllers;
 use App\Services\LineBotService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use LINE\LINEBot\MessageBuilder;
+use LINE\LINEBot;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 class LineController
 {
@@ -16,20 +19,27 @@ class LineController
     private $lineBotService;
 
     /**
+     * @var LINEBot
+     */
+    private $LINEBot;
+
+    /**
      * LineController constructor.
      * @param LineBotService $lineBotService
+     * @param LINEBot $LINEBot
      */
-    public function __construct(LineBotService $lineBotService)
+    public function __construct(LineBotService $lineBotService, LINEBot $LINEBot)
     {
         $this->lineBotService = $lineBotService;
+        $this->LINEBot = $LINEBot;
     }
 
     /**
      * @param Request $request
-     * @return Response
+     * @return LINEBot\Response
      */
     public function line(Request $request)
     {
-        return response('okk');
+        return $this->LINEBot->pushMessage(env('LINE_USER_ID'), new TextMessageBuilder('aaa'));
     }
 }
